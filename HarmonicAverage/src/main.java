50d533460e0bd9b46bac3c24a85bd5c040f75e01
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-10 14:51:37 +0300

    harmonic average

--- HarmonicAverage/src/main.java
+++ HarmonicAverage/src/main.java
@@ -1,11 +1,11 @@
 public class main {
     public static void main(String[] args) {
 
-        int[] numbers = {12,4,6,32,21,9,3};
-        double sum=0.0;
+        int[] numbers = {1,2,3,4,5,6};
+        double sum = 0.0;
 
-        for (int i=0;i<numbers.length;i++){
-            sum+=1/(1.0+numbers[i]);
+        for (int i = 0; i < numbers.length; i++){
+            sum += 1/(1.0+numbers[i]);
         }
 
         double average = sum / numbers.length;
